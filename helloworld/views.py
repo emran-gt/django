b318aefe49e2bbfc1b1f83ac2aa8f30f65a910bb
Rokon <rokon@itebd.com>
2020-09-01 17:38:50 +0600

    first

--- helloworld/views.py
+++ helloworld/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world! Master Branch. Congratulations!!!!!!")
+    return HttpResponse("Hello, world! This is Master Branch!!")
